5583f2efd0f89bca2ad7854f15e9a1e2019960d8
Repo <duongquangsang080299@gmail.com>
2023-07-06 08:26:50 +0700

    update

--- Exercism/dart/high-scores/lib/high_scores.dart
+++
@@ -1,6 +0,0 @@
-class HighScores {
-  // Using constructor is defined by the name of class
-
-  List<int> scores;
-  HighScores(this.scores); //put scores parameter in this bracket
-}
--- Exercism/dart/two-fer/lib/two_fer.dart
+++
@@ -1,5 +0,0 @@
-String twoFer([String name='you']) {
-  // Replace the throw call and put your code here
-  return "One for $name, one for me.";
-  
-}
