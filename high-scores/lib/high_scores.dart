c8f9d1edde87486ebfb66aa1f5d5cfe9e14ed27e
duongquangsang080299 <53465403+duongquangsang080299@users.noreply.github.com>
2023-07-10 12:14:17 +0700

    update word count

--- high-scores/lib/high_scores.dart
+++
@@ -1,9 +0,0 @@
-class HighScores {
-  List <int> scores;
-
-  HighScores (this.scores);
-  int latest () => scores.last;
-  List<int> sortList () => scores.toList()..sort();
-  int personalBest () => sortList().last;
-  List<int> personalTopThree() => sortList().reversed.take(3).toList();
-}
