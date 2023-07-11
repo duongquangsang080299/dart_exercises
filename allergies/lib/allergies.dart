29bedb2fc2e9895c26371f2deca00442886aa650
duongquangsang080299 <53465403+duongquangsang080299@users.noreply.github.com>
2023-07-11 16:25:53 +0700

    implement the example of allergies

---
+++ allergies/lib/allergies.dart
@@ -0,0 +1,16 @@
+class Allergies {
+  static const allergies = const {
+    "eggs": 1,
+    "peanuts": 2,
+    "shellfish": 4,
+    "strawberries": 8,
+    "tomatoes": 16,
+    "chocolate": 32,
+    "pollen": 64,
+    "cats": 128
+  };
+  bool allergicTo(String aliment, int score) =>
+      score & allergies[aliment]! != 0;
+  List<String> list(int score) =>
+      allergies.keys.where((aliment) => allergicTo(aliment, score)).toList();
+}
